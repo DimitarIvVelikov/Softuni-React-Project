2f12669cf6aa31a1c90e9abe25f467c052d2700e
DimitarIvVelikov <74976492+DimitarIvVelikov@users.noreply.github.com>
2024-07-31 11:45:05 +0300

    Added home page

---
+++ client/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <div className="img-container">
+        <h1 className="home-title">Welcome to the best fitness forum</h1>
+        <h3 className="home-title-sub">Get to your dream physic!</h3>
+      </div>
+    </>
+  );
+}
