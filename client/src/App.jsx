1dbac43024250c99a192064af0429916708d5d55
DimitarIvVelikov <74976492+DimitarIvVelikov@users.noreply.github.com>
2024-07-31 11:45:49 +0300

    Added routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
+import Home from "./components/home/Home";
 
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
