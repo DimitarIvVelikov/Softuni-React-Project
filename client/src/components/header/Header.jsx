bbb760934c956fa33a63d06d8bea0600ecf308de
DimitarIvVelikov <74976492+DimitarIvVelikov@users.noreply.github.com>
2024-07-31 11:45:17 +0300

    Added header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,23 +1,37 @@
 import { Link } from "react-router-dom";
-import styles from "./Header.module.css";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
 
 export default function Header() {
   return (
     <>
       <nav className="header">
-        <img src="logo.jpg" alt="#" className={styles.logo} />
+        <Link to="/" className="logo">
+          <FitnessCenterIcon fontSize="large" />
+        </Link>
 
         <ul className="main-nav">
-          <li>
-            <Link to="/">Home</Link>
+          <li className="main-nav-li">
+            <Link to="/" className="main-nav-link">
+              Home
+            </Link>
           </li>
-          <li>
-            <Link to="/catalog">Catalog</Link>
+          <li className="main-nav-li">
+            <Link to="/catalog" className="main-nav-link">
+              Catalog
+            </Link>
           </li>
-          <li>
-            <Link to="/">Contact</Link>
+          <li className="main-nav-li">
+            <Link to="/" className="main-nav-link">
+              Contact
+            </Link>
           </li>
         </ul>
+
+        <p className="username">
+          <Link to="/" className="username-link">
+            User
+          </Link>
+        </p>
       </nav>
     </>
   );
