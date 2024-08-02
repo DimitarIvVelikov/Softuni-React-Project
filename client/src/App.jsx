bf67b11b536c019124010e83ca6c621cd1b37677
DimitarIvVelikov <74976492+DimitarIvVelikov@users.noreply.github.com>
2024-08-03 01:39:22 +0300

    Added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,9 @@ import "./App.css";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
+import Catalog from "./components/catalog/Catalog";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/catalog" element={<Catalog />} />
       </Routes>
       <Footer />
     </>
