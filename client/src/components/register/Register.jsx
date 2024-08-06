beb27d9aff8dfa03f8bd00da1adf6ccffbd4e0f3
DimitarIvVelikov <74976492+DimitarIvVelikov@users.noreply.github.com>
2024-08-06 09:42:37 +0300

    Modified Register Page

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,14 +1,37 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { userService } from "../../apiAndServices/userService";
+import { useAuthContext } from "../../context/AuthContext";
 
 export default function Register() {
   const [formValues, setFormValues] = useState({
+    username: "",
     email: "",
     password: "",
-    repeatPassword: "",
+    rePassword: "",
   });
+  const navigate = useNavigate();
+  const { changeAuthState } = useAuthContext();
 
-  const submitHandler = (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault();
+
+    try {
+      const { username, email, accessToken, _id } = await userService.register(
+        formValues
+      );
+
+      changeAuthState({
+        userId: _id,
+        email,
+        accessToken,
+        username,
+        isAuthenticated: true,
+      });
+      navigate("/");
+    } catch (error) {
+      console.log("Error Register");
+    }
   };
 
   const onChangeHandler = (e) => {
@@ -23,6 +46,19 @@ export default function Register() {
       <h1 className="home-title">Sign Up</h1>
 
       <form onSubmit={submitHandler} className="login-form">
+        <div className="username-input">
+          <label htmlFor="username">
+            Username <span className="required">*</span>
+          </label>
+          <input
+            type="username"
+            id="username"
+            name="username"
+            placeholder="username"
+            value={formValues.username}
+            onChange={onChangeHandler}
+          />
+        </div>
         <div className="email-input">
           <label htmlFor="email">
             Email <span className="required">*</span>
@@ -50,15 +86,15 @@ export default function Register() {
           />
         </div>
         <div className="repeat-password-input">
-          <label htmlFor="password">
+          <label htmlFor="rePassword">
             Repeat Password <span className="required">*</span>
           </label>
           <input
-            type="password"
-            id="repeatPassword"
-            name="repeatPassword"
+            type="rePassword"
+            id="rePassword"
+            name="rePassword"
             placeholder="Repeat Password"
-            value={formValues.repeatPassword}
+            value={formValues.rePassword}
             onChange={onChangeHandler}
           />
         </div>
