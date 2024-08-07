adce24da87c2be793e17efe6f00fb9fa7b172107
DimitarIvVelikov <74976492+DimitarIvVelikov@users.noreply.github.com>
2024-08-07 16:22:24 +0300

    Added multiple API request handlers

--- client/src/apiAndServices/dataService.js
+++ client/src/apiAndServices/dataService.js
@@ -2,10 +2,18 @@ import { api } from "./api";
 const URI = "training-programs";
 const dataEndpoints = {
   baseTrainingPrograms: URI + "/",
+  trainingProgram: (programId) => URI + `/${programId}`,
+  latestTrainings: URI + "/latest",
+  signUpForTrainingProgram: (programId) => URI + `/signUp/${programId}`,
 };
 
 function getAllTrainingPrograms() {
   const data = api.get(dataEndpoints.baseTrainingPrograms);
+  return data;
+}
+
+function getTrainingProgram(programId) {
+  const data = api.get(dataEndpoints.trainingProgram(programId));
   return data;
 }
 
@@ -17,7 +25,34 @@ function createTrainingProgram(trainingProgram) {
   return createdProgram;
 }
 
+function editTrainingProgram(programId, trainingProgram) {
+  const data = api.put(
+    dataEndpoints.trainingProgram(programId),
+    trainingProgram
+  );
+  return data;
+}
+
+function deleteTrainingProgram(programId) {
+  const data = api.del(dataEndpoints.trainingProgram(programId));
+  return data;
+}
+
+function getLatestTrainingPrograms() {
+  const data = api.get(dataEndpoints.latestTrainings);
+  return data;
+}
+
+function signUpForTrainingProgram(programId) {
+  api.post(dataEndpoints.signUpForTrainingProgram(programId));
+}
+
 export const dataService = {
   getAllTrainingPrograms,
   createTrainingProgram,
+  getTrainingProgram,
+  editTrainingProgram,
+  deleteTrainingProgram,
+  getLatestTrainingPrograms,
+  signUpForTrainingProgram,
 };
