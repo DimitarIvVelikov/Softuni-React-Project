ff574656e285abb354cc129d4f54e298e59bf2c6
DimitarIvVelikov <74976492+DimitarIvVelikov@users.noreply.github.com>
2024-08-07 16:20:52 +0300

    Added Logout and conditional rendering

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,9 +1,28 @@
 import { Link } from "react-router-dom";
 import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
 import { useAuthContext } from "../../context/AuthContext";
+import { userService } from "../../apiAndServices/userService";
 
 export default function Header() {
-  const { username } = useAuthContext();
+  const { username, changeAuthState, isAuthenticated } = useAuthContext();
+
+  const logoutHandler = () => {
+    try {
+      userService.logout();
+      changeAuthState({
+        isAuthenticated: false,
+        username: "",
+        _id: "",
+        accessToken: "",
+        email: "",
+        changeAuthState,
+      }); // Clear user data from the auth context
+      localStorage.removeItem("auth");
+    } catch (error) {
+      console.log("Error logout");
+      console.log(error);
+    }
+  };
 
   return (
     <>
@@ -14,30 +33,37 @@ export default function Header() {
 
         <ul className="main-nav">
           <li className="main-nav-li">
-            <Link to="/" className="main-nav-link">
-              Home
-            </Link>
-          </li>
-          <li className="main-nav-li">
             <Link to="/catalog" className="main-nav-link">
               Catalog
             </Link>
           </li>
-          <li className="main-nav-li">
-            <Link to="/login" className="main-nav-link">
-              Login
-            </Link>
-          </li>
-          <li className="main-nav-li">
-            <Link to="/register" className="main-nav-link">
-              Register
-            </Link>
-          </li>
-          <li className="main-nav-li">
-            <Link to="/create" className="main-nav-link">
-              Create
-            </Link>
-          </li>
+          {!isAuthenticated ? (
+            <>
+              <li className="main-nav-li">
+                <Link to="/login" className="main-nav-link">
+                  Login
+                </Link>
+              </li>
+              <li className="main-nav-li">
+                <Link to="/register" className="main-nav-link">
+                  Register
+                </Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className="main-nav-li">
+                <Link to="/create" className="main-nav-link">
+                  Create
+                </Link>
+              </li>
+              <li className="main-nav-li">
+                <Link to="/" className="main-nav-link" onClick={logoutHandler}>
+                  Logout
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
 
         <p className="username">
